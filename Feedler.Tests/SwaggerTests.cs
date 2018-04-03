ab7ca456a5eb10a856f9a02912cdb697b2ddde1c
Oleksii Bespalov <alexb5dh@gmail.com>
2018-04-04 01:31:20 +0300

    add tests for Swagger

---
+++ Feedler.Tests/SwaggerTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+using System.Threading.Tasks;
+using Feedler.Tests.Extensions;
+using Xunit;
+
+namespace Feedler.Tests
+{
+    /// <summary>
+    /// Tests to ensure Swagger schema is not broken and UI is working as intended.
+    /// </summary>
+    public class SwaggerTests: FeedlerTestHost
+    {
+        [TestFor("Swagger")]
+        public async Task Swagger_ShouldHaveUI()
+        {
+            var response = await Client.GetAsync("swagger/");
+            Assert.Equal(expected: HttpStatusCode.OK, actual: response.StatusCode);
+        }
+
+        [TestFor("Swagger")]
+        public async Task Swagger_ShouldHaveSchema()
+        {
+            var response = await Client.GetAsync("swagger/v1/swagger.json");
+            Assert.Equal(expected: HttpStatusCode.OK, actual: response.StatusCode);
+        }
+    }
+}
